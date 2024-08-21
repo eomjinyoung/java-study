f85a1a6707c91f739c9d195b234b4cb61afe735c
eomjinyoung <jinyoung.eom@gmail.com>
2024-08-21 23:11:55 +0900

    .

---
+++ java-lang/src/main/java/operator/Test51.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class Test51 {
+  public static void main(String[] args) {
+    // 비트 연산자
+    System.out.println(0b0011 & 0b0101); // 0001 = 1
+    System.out.println(0b0011 | 0b0101); // 0111 = 7
+    System.out.println(0b0011 ^ 0b0101); // 0110 = 6
+    System.out.println(~0b0011); // 1100 = -4
+
+  }
+}
