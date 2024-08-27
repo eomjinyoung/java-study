058b7c2fe0a8643fe70d4df669b79cd6eba29554
eomjinyoung <jinyoung.eom@gmail.com>
2024-08-27 23:52:42 +0900

    .

---
+++ java-lang/src/main/java/condition/Test31.java
@@ -0,0 +1,29 @@
+package condition;
+
+public class Test31 {
+  public static void main(String[] args) {
+    // switch ~ case 문
+    // switch (int 정수, 문자열, Enum 타입) {
+    // case 상수, 리터럴:
+    // ...
+    // break;
+    // ...
+    // default:
+    // }
+
+    switch (91) {
+      case 100:
+        System.out.println("A");
+        break;
+      case 90:
+        System.out.println("B");
+        break;
+      case 80:
+        System.out.println("C");
+        break;
+      default:
+        System.out.println("F");
+    }
+
+  }
+}
